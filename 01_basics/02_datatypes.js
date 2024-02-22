953f08696327993d78060b22cee7bda3ef381d09
Ashish kumar <160783678+Ashish664811@users.noreply.github.com>
2024-02-22 19:04:44 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) we are using node js not browser so we can not use alert here
+
+console.log(3+3);
+
+//code readability should be high
+console.log("Ashish")
+
+let name="Ashish" //string data type
+let age=18 //number (int) data type
+let isLoggedIn=false //boolean data type
+
+  //premetive datatype
+//number=> reange 2 to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+//null=>standallone value
+//undefined=>
+//symbol=> for unique things
+
+
+
+//object
+
+console.log(typeof undefined);//output ->undefined
+console.log(typeof null);//output-> object
+
