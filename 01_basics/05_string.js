d9355c4544e74ce9bc5f262ce3ba9bc7af4ae00c
Ashish kumar <160783678+Ashish664811@users.noreply.github.com>
2024-02-24 05:28:37 +0000

    05_string commit

---
+++ 01_basics/05_string.js
@@ -0,0 +1,40 @@
+const name="Ashish"
+const repoCount=2
+
+//we not use this type of concattation
+//console.log(name + repoCount + "value");
+
+//we use back tex(``) for concatation
+
+//console.log(`Hello My name is ${name} and my repe count is ${repoCount}`);
+
+const gameName=new String('AsHish-kumar-raj-bunny-and-me')
+
+// console.log(gameName[2])
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+
+// console.log(gameName);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.toLocaleLowerCase());
+
+// console.log(gameName.charAt(2));
+
+// const newString=gameName.slice(-6,-2);
+// console.log(newString);
+
+// const newString ="  Ashish  "
+// console.log(newString)
+// console.log(newString.trim());
+
+
+const url="https://ashish.com/ashish%20kumar"
+
+// console.log(url.replace('%20','--'));
+
+// console.log(url.includes('ashish'));
+
+console.log(gameName.split('-',4));
+
+
