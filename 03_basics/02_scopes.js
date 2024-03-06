40bb560d45088be0115cbb571a4bd60af67a6b73
Ashish kumar <160783678+Ashish664811@users.noreply.github.com>
2024-03-06 11:56:28 +0000

    scope commit

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -21,3 +21,49 @@ if(true) {
 // c is defind in side if then it have to show error
 // not it work vecause var type is accessible out side
 // of scope
+
+function one(){
+    const userName = "hitesh";
+
+    function two(){
+        const website = "youtub";
+        console.log(userName);
+    }
+
+    //console.log(website);//website is not defined
+    two();//hitesh
+}
+
+//one();
+
+
+if(true){
+    const userName="Ashish";
+    if(userName==="Ashish"){
+        const website = " youtube";
+        //console.log(userName+website);//Ashish youtube
+    }
+
+    //console.log(website);//website is not defined
+}
+
+//console.log(userName);//userName is not defined
+
+//+++++++++++++++++ interisting ++++++++++++++++++++++
+//addOne ko function ke upar kahi se v call kar skta hu
+// kyo ki usko as a function declare kiya gya hi
+function addOne(Num){
+    return Num+1;
+}
+
+console.log(addOne(5));
+
+
+// addTwo ko function ke upar call karne pe error aata
+//hi kyo yaha addTwo function ke sath sath ek varible 
+// v hi jo function ke upar diclare nahi hi
+const addTwo = function(num){
+    return num+2;
+}
+
+console.log(addTwo(5));
